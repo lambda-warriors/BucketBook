6741a9c4a2cd47e1ba55833c320538775df38de4
Asha <ashalbenjamin@gmail.com>
2019-08-28 21:53:02 -0500

    added-routes-to-app

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import Login from './components/Login';
 import TopBar from './components/TopBar';
 import Dashboard from './components/Dashboard';
-import {Route} from 'react-router-dom';
+import Signup from '/.components/Signup';
+import { Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <TopBar/>
-      <Route exact path='/' component={Login}/>
-      <Route path='/dashboard/' component={Dashboard}/>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<TopBar />
+			<Route exact path='/' component={Login} />
+			<Route path='/dashboard/' component={Dashboard} />
+			<Route path='/signup/' component={Signup} />
+		</div>
+	);
 }
 
 export default App;
